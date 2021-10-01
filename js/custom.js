a0db91661303ee6d56baf98cc0e2d5914d4fd272
MLMridul <mdmridul344@gmail.com>
2021-10-02 01:27:10 +0600

    Code Add

--- js/custom.js
+++ js/custom.js
@@ -23,5 +23,34 @@ $(document).ready(function () {
         arrows: false,
     });
 
+    // Testimonial SLider
+    $('.testimonial-slider').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+    });
+
+    // Testimonial SLider
+    $('.brand-slider').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        centerMode: true,
+        centerPadding: '0px',
+        prevArrow: '<i class="fas fa-angle-left"></i>',
+        nextArrow: '<i class="fas fa-angle-right"></i>'
+    });
+
+    // Counter Up
+
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
 
 })
