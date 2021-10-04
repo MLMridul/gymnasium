6c8e86d9c97245a938ecbb95def3277dec305ee3
MLMridul <mdmridul344@gmail.com>
2021-10-04 14:09:24 +0600

    All Done

--- js/custom.js
+++ js/custom.js
@@ -5,7 +5,25 @@ $(document).ready(function () {
     $('.preloader').delay(1000).slideUp(2000);
     $('.loader').fadeOut(1000);
 
+    //animation scroll js
+    var html_body = $('html, body');
+    $('nav a').on('click', function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                html_body.animate({
+                    scrollTop: target.offset().top - 0
+                }, 1500, );
+                return false;
+            }
+        }
+    });
 
+
+    // Wow Js
+
+    new WOW().init();
 
     // ScroolTop
 
